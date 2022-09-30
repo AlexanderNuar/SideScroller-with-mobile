b5ed07a26987174c8055a79bd1c90f5804594692
AlexanderNuar <101461783+AlexanderNuar@users.noreply.github.com>
2022-09-30 20:53:28 +0300

    3

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ window.addEventListener("DOMContentLoaded", function () {
         console.log(e.key, this.keys);
       });
       window.addEventListener("keyup", (e) => {
-        if (e.key === "ArrowDown" && this.keys.indexOf(e.key) === -1) {
-          this.keys.push(e.key);
+        if (e.key === "ArrowDown") {
+          this.keys.splice(this.keys.indexOf(e.key), 1);
         }
         console.log(e.key, this.keys);
       });

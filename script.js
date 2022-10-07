f948be5f3a6cdbe62937641084b1b463965e27fc
AlexanderNuar <101461783+AlexanderNuar@users.noreply.github.com>
2022-10-07 15:11:45 +0300

    07 oct 1

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ window.addEventListener("DOMContentLoaded", function () {
   class inputHandler {
     constructor() {
       this.keys = [];
+      this.touchY = "";
+      this.touchTreshold = 30;
       window.addEventListener("keydown", (e) => {
         if (
           (e.key === "ArrowDown" ||
@@ -33,6 +35,25 @@ window.addEventListener("DOMContentLoaded", function () {
         ) {
           this.keys.splice(this.keys.indexOf(e.key), 1);
         }
+      });
+      window.addEventListener("touchstart", (e) => {
+        this.touchY = e.changedTouches[0].pageY;
+      });
+      window.addEventListener("touchmove", (e) => {
+        const swipeDistance = e.changedTouches[0].pageY - this.touchY;
+        if (
+          swipeDistance < -this.touchTreshold &&
+          this.keys.indexOf("swipe up") === -1
+        )
+          this.keys.push("swipe up");
+        else if (
+          swipeDistance > this.touchTreshold &&
+          this.keys.indexOf("swipe down") === -1
+        )
+          this.keys.push("swipe down");
+      });
+      window.addEventListener("touchend", (e) => {
+        console.log(this.keys);
       });
     }
   }
